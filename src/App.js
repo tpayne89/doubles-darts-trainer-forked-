53153a48832929626d07a257dc6047e89678b4bd
tpayne89 <86229366+tpayne89@users.noreply.github.com>
2025-07-08 14:23:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -551,7 +551,7 @@ export default function App() {
           style={{
             display: "flex",
             justifyContent: "center",
-            marginTop: "1rem",
+            marginTop: "-40px", // negative margin to pull up
           }}
         >
           <DartboardHeatmap stats={statsByDouble} />
