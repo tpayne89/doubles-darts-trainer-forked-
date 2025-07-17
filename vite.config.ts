273d962a01c1ef08c0e9cf6e87d62a27ba9cfa13
tpayne89 <86229366+tpayne89@users.noreply.github.com>
2025-07-17 13:41:02 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: ["nf6p4w-5173.csb.app"],
+  },
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, "public/index.html"),
+    },
   },
 });
