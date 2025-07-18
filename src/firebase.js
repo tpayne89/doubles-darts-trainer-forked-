399afbca2f143b791292dbdf01cc8ed1bad7dd0e
tpayne89 <86229366+tpayne89@users.noreply.github.com>
2025-07-18 11:30:56 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAnKQVAgQQaeujXq2z2yQWoqLMVqneYE30",
@@ -8,10 +9,17 @@ const firebaseConfig = {
   storageBucket: "dartsdoublestrainer.firebasestorage.app",
   messagingSenderId: "471422765052",
   appId: "1:471422765052:web:df629d251b0a1a6e0b1479",
-  measurementId: "G-VG9GZKE97H"
+  measurementId: "G-VG9GZKE97H",
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-export { app, analytics };
+let analyticsPromise = isSupported().then((supported) => {
+  if (supported) {
+    return getAnalytics(app);
+  } else {
+    return null;
+  }
+});
+
+export { app, analyticsPromise };
